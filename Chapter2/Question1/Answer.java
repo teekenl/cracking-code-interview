90b87ca3ecf29fa39d8beea2e8676b58f5b76993
teekenl <dknight1938@hotmail.com>
2017-09-27 22:32:49 +1000

    util/Node is created

---
+++ Chapter2/Question1/Answer.java
@@ -0,0 +1,13 @@
+package Chapter2.Question1;
+
+import java.util.*;
+
+public class Answer {
+    public Answer() {
+
+    }
+
+    public static void main (String[] args) {
+
+    }
+}
---
+++ Chapter2/Question1/util/Node.java
@@ -0,0 +1,31 @@
+package Chapter2.Question1.util;
+
+
+public class Node {
+    public Node next = null;
+    public int data;
+    public Node(int d) {
+        this.data = d;
+    }
+    public void appendToTail(int d) {
+        Node end = new Node(d);
+        Node n = this;
+        while(n.next != null) { n = n.next; }
+        n.next = end;
+    }
+
+    public Node deleteNode (Node head, int d) {
+        Node n = head;
+        if( n.data == d) {
+            return head.next;
+        }
+        while( n.next  != null) {
+            if(n.next.data  == d) {
+                n.next = n.next.next;
+                return head;
+            }
+            n = n.next;
+        }
+        return null;
+    }
+}
