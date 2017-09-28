15add6805e24ee83e232638c244e780617bf9fba
teekenl <dknight1938@hotmail.com>
2017-09-28 16:48:11 +1000

    Chapter2 Question 1 Answer

--- Chapter2/Question1/Answer.java
+++ Chapter2/Question1/Answer.java
@@ -1,5 +1,6 @@
 package Chapter2.Question1;
 
+import Chapter2.Question1.util.*;
 import java.util.*;
 
 public class Answer {
@@ -7,7 +8,50 @@ public class Answer {
 
     }
 
+    public static void deleteDuplicates(Node n) {
+        Hashtable table = new Hashtable();
+        Node previous = n;
+        while (n != null) {
+            if (table.containsKey(n.data)) {
+                previous.next = n.next;
+            } else {
+                table.put(n.data, true);
+                previous = n;
+            }
+            n = n.next;
+        }
+    }
+
+    // No buffer : No hashmap & hashtable
+    public static void deleteDuplicatesNoBuffer(Node first) {
+        if(first == null) return;
+        Node previous = first;
+        Node current = first.next;
+        while(current != null) {
+            Node iterate = first;
+            while(iterate != current) {
+                if(iterate.data == current.data) {
+                    Node tmp = current.next;
+                    previous.next = tmp;
+                    current = tmp;
+                    break;
+                }
+                iterate = iterate.next;
+            }
+            if(iterate ==  current) {
+               previous = current;
+               current = current.next;
+            }
+        }
+    }
+
     public static void main (String[] args) {
+        Node n = RandomNodeList.randomNode(10,0,10);
+        System.out.println("Node list with duplicates: " + n.printNodeList());
+        // Remove duplicates
+        deleteDuplicatesNoBuffer(n);
+        System.out.println("Result of node -> " + n.printNodeList());
 
     }
+
 }
--- Chapter2/Question1/util/Node.java
+++ Chapter2/Question1/util/Node.java
@@ -2,16 +2,33 @@ package Chapter2.Question1.util;
 
 
 public class Node {
-    public Node next = null;
+    public Node previous;
+    public Node next;
     public int data;
-    public Node(int d) {
+    public Node(int d, Node prev, Node next) {
         this.data = d;
+        this.setPreviousNode(prev);
+        this.setNextNode(next);
     }
     public void appendToTail(int d) {
-        Node end = new Node(d);
+        Node end = new Node(d, this,null);
         Node n = this;
         while(n.next != null) { n = n.next; }
         n.next = end;
+    }
+
+    public void setNextNode(Node n ) {
+        this.next = n;
+        if (n != null && n.previous != this) {
+            n.setPreviousNode(this);
+        }
+    }
+
+    public void setPreviousNode(Node n ) {
+        this.previous = n;
+        if( n != null && n.next != this) {
+            n.setNextNode(this);
+        }
     }
 
     public Node deleteNode (Node head, int d) {
@@ -27,5 +44,13 @@ public class Node {
             n = n.next;
         }
         return null;
+    }
+
+    public String printNodeList() {
+        if(this.next != null) {
+            return data + " -> " + this.next.printNodeList();
+        } else{
+            return ((Integer) data).toString();
+        }
     }
 }
